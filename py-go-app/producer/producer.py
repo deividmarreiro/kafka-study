1c21a3884d68f148870617833804f35525b61a59
deivid.marreiro <davidteixeiro@gmail.com>
2022-04-24 13:27:24 -0300

    Creation of producer in python Starting ReadME and TODO List with Related Links

---
+++ py-go-app/producer/producer.py
@@ -0,0 +1,11 @@
+import json
+import random
+
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:29092',
+                         value_serializer=lambda message: json.dumps(message).encode('utf-8'))
+
+print("Ctrl+c to Stop!")
+while True:
+    producer.send('kafka-python-topic', random.randint(1, 999))
